b17d78a3aeb183a6f3e86067458db2321cea5754
Garen Yondem <garen@yondem.com>
2020-03-12 03:25:45 +0300

    Add custom String interface for custom extensions

--- src/extensions/stringExtensions.ts
+++ src/extensions/stringExtensions.ts
@@ -1,3 +1,7 @@
-String.prototype.toDate = function(this: string): Date {
+interface String {
+    toDate(this: string): Date;
+}
+
+String.prototype.toDate = function (this: string): Date {
     return new Date(this);
 };
